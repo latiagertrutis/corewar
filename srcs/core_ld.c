54b863173e6968d689c8f6b154e213eb8656e07d
Mateo RODRIGUEZ <mrodrigu@e3r5p15.42.fr>
2018-07-13 12:16:31 +0200

    core_ld mejorado y algunos comentarios

--- srcs/core_ld.c
+++ srcs/core_ld.c
@@ -6,7 +6,7 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 03:43:25 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/07/13 10:55:22 by mrodrigu         ###   ########.fr       */
+/*   Updated: 2018/07/13 12:15:32 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,43 +17,88 @@
 **       1arg 2arg 3arg   no susa
 */
 
-static void		load_data(t_player *player, t_board *board, unsigned short pos, int ocp)
-{
-	unsigned char	reg_pos;
-	unsigned char	*board_pos;
-	unsigned char	*buff;
+/* static void		load_data(t_player *player, t_board *board, unsigned short pos, int ocp) */
+/* { */
+/* 	unsigned char	reg_pos; */
+/* 	unsigned char	*board_pos; */
+/* 	unsigned char	*buff; */
 
-	buff = NULL;
-	board_pos = NULL;
-	if (ocp == 0x90 && (reg_pos = (*(board + pos + REG_SIZE)).mem - 1) < REG_NUMBER)
-	{
-		buff = get_mem_board(board + pos, REG_SIZE);//obtener el valor del directo
-		ft_memcpy(player->reg[reg_pos], buff, REG_SIZE);
-		player->pc += 1 + 1 + 1 + REG_SIZE;//ld + opc + reg + ind
+/* 	buff = NULL; */
+/* 	board_pos = NULL; */
+/* 	if (ocp == 0x90 && (reg_pos = (*(board + pos + REG_SIZE)).mem - 1) < REG_NUMBER) */
+/* 	{ */
+/* 		buff = get_mem_board(board + pos, REG_SIZE);//obtener el valor del directo */
+/* 		ft_memcpy(player->reg[reg_pos], buff, REG_SIZE); */
+/* 		player->pc += 1 + 1 + 1 + REG_SIZE;//ld + opc + reg + ind */
+/* 	} */
+/* 	else if (ocp == 0xD0 && (reg_pos = (*(board + pos + IND_SIZE)).mem - 1) < REG_NUMBER) */
+/* 	{ */
+/* 		board_pos = get_mem_board(board + pos, IND_SIZE);//obtener el valor del indirecto */
+/* 		invert_bytes(board_pos, IND_SIZE); */
+/* 		buff = get_mem_board(board + ((*((unsigned short *)board_pos) */
+/* 		+ player->pc) % IDX_MOD), REG_SIZE);//acceder a la posicion (pc + indirecto) % idx */
+/* 		ft_memcpy(player->reg[reg_pos], buff, REG_SIZE); */
+/* 		free(board_pos); */
+/* 		player->pc += 1 + 1 + 1 + IND_SIZE;//ld + opc + reg + dir */
+/* 	} */
+/* 	player->carry = (buff && !*((int *)buff)) ? 0x1 : 0x0; */
+/* 	free(buff); */
+/* } */
+
+/*
+** POSSIBLE CAST TO GENERIC
+*/
+
+static void		load_direct(t_player *player, t_board *board, const unsigned char reg_pos, unsigned short pos)
+{
+	unsigned char	i;
+
+	i = 0;
+	while (i < REG_SIZE)
+	{//copia de lo que hay en el mapa en el registro
+		player->reg[reg_pos][i] = (*(board + pos + i)).mem;
+		i++;
 	}
-	else if (ocp == 0xD0 && (reg_pos = (*(board + pos + IND_SIZE)).mem - 1) < REG_NUMBER)
-	{
-		board_pos = get_mem_board(board + pos, IND_SIZE);//obtener el valor del indirecto
-		invert_bytes(board_pos, IND_SIZE);
-		buff = get_mem_board(board + ((*((unsigned short *)board_pos)
-		+ player->pc) % IDX_MOD), REG_SIZE);//acceder a la posicion (pc + indirecto) % idx
-		ft_memcpy(player->reg[reg_pos], buff, REG_SIZE);
-		free(board_pos);
-		player->pc += 1 + 1 + 1 + IND_SIZE;//ld + opc + reg + dir
+	player->pc += 1 + 1 + 1 + REG_SIZE;//ld + opc + reg + ind
+	player->carry = (!*((int *)(player->reg[reg_pos]))) ? 0x1 : 0x0;//actualizar carry
+}
+
+static void		load_indirect(t_player *player, t_board *board, unsigned char reg_pos, unsigned short pos)
+{
+	unsigned char	board_pos[IND_SIZE];//el indirecto (2)
+	unsigned char	i;
+
+	i = 0;
+	while (i < IND_SIZE)
+	{//copia de lo que hay en el mapa en board_pos
+		board_pos[IND_SIZE - 1 - i] = (*(board + pos + i)).mem;
+		i++;
 	}
-	player->carry = (buff && !*((int *)buff)) ? 0x1 : 0x0;
-	free(buff);
+	i = 0;
+	while (i < REG_SIZE)
+	{//accediendo mediante el anterior indirecto se copia en el registro lo apuntado
+		player->reg[reg_pos][i] = (*(board + i + ((*((unsigned short *)board_pos)
+									+ player->pc) % IDX_MOD))).mem;
+		i++;
+	}
+	player->pc += 1 + 1 + 1 + IND_SIZE;//ld + opc + reg + dir
+	player->carry = (!*((int *)(player->reg[reg_pos]))) ? 0x1 : 0x0;//actualizar carry
 }
 
 void			core_ld(t_player *player, t_op op, t_arena *arena)
 {
 	unsigned short 	pos;
 	unsigned char	ocp;
+	unsigned char	reg_pos;
 
 	pos = player->pc;
-	ocp = arena->board[++pos].mem;
-	load_data(player, arena->board, ++pos, ocp);
-//	print_memory(player->reg[(*(arena->board + pos + IND_SIZE)).mem - 1], 4, 16, 1);
-//	print_memory(player->reg[(*(arena->board + pos + REG_SIZE)).mem - 1], 4, 16, 1);
-//	exit(1);
+	ocp = arena->board[pos + 1].mem;//en pc + 1 esta ocp y en pc + 2 esta el primer argumento
+	if (ocp == 0x90 && (reg_pos = (*(arena->board + (pos + 2) + REG_SIZE)).mem - 1) < REG_NUMBER)
+		load_direct(player, arena->board, reg_pos, pos + 2);
+	else if (ocp == 0xD0 && (reg_pos = (*(arena->board + (pos + 2) + IND_SIZE)).mem - 1) < REG_NUMBER)
+		load_indirect(player, arena->board, reg_pos, pos + 2);
+//	load_data(player, arena->board, ++pos, ocp);
+//	print_memory(player->reg[(*(arena->board + (pos + 2) + IND_SIZE)).mem - 1], 4, 16, 1);
+//	print_memory(player->reg[(*(arena->board + (pos + 2) + REG_SIZE)).mem - 1], 4, 16, 1);
+	exit(1);
 }
--- zork_f_prueba.s
+++ zork_f_prueba.s
@@ -1,5 +1,5 @@
 .name "zork"
 .comment "I'M ALIIIIVE"
 
-l2:	ld %1023, r7
-	st r7, 1023
+l2:	ld 3, r7
+	st r7, 50
