5c6943a5915c5f38dd940d4672ba89abdb0e611c
Mateo RODRIGUEZ <mrodrigu@e3r5p15.42.fr>
2018-07-13 12:33:43 +0200

    fuera comentario

--- srcs/core_ld.c
+++ srcs/core_ld.c
@@ -6,7 +6,7 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 03:43:25 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/07/13 12:15:32 by mrodrigu         ###   ########.fr       */
+/*   Updated: 2018/07/13 12:33:20 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,6 @@
 ** ocp =  11   11   11      00
 **       1arg 2arg 3arg   no susa
 */
-
-/* static void		load_data(t_player *player, t_board *board, unsigned short pos, int ocp) */
-/* { */
-/* 	unsigned char	reg_pos; */
-/* 	unsigned char	*board_pos; */
-/* 	unsigned char	*buff; */
-
-/* 	buff = NULL; */
-/* 	board_pos = NULL; */
-/* 	if (ocp == 0x90 && (reg_pos = (*(board + pos + REG_SIZE)).mem - 1) < REG_NUMBER) */
-/* 	{ */
-/* 		buff = get_mem_board(board + pos, REG_SIZE);//obtener el valor del directo */
-/* 		ft_memcpy(player->reg[reg_pos], buff, REG_SIZE); */
-/* 		player->pc += 1 + 1 + 1 + REG_SIZE;//ld + opc + reg + ind */
-/* 	} */
-/* 	else if (ocp == 0xD0 && (reg_pos = (*(board + pos + IND_SIZE)).mem - 1) < REG_NUMBER) */
-/* 	{ */
-/* 		board_pos = get_mem_board(board + pos, IND_SIZE);//obtener el valor del indirecto */
-/* 		invert_bytes(board_pos, IND_SIZE); */
-/* 		buff = get_mem_board(board + ((*((unsigned short *)board_pos) */
-/* 		+ player->pc) % IDX_MOD), REG_SIZE);//acceder a la posicion (pc + indirecto) % idx */
-/* 		ft_memcpy(player->reg[reg_pos], buff, REG_SIZE); */
-/* 		free(board_pos); */
-/* 		player->pc += 1 + 1 + 1 + IND_SIZE;//ld + opc + reg + dir */
-/* 	} */
-/* 	player->carry = (buff && !*((int *)buff)) ? 0x1 : 0x0; */
-/* 	free(buff); */
-/* } */
 
 /*
 ** POSSIBLE CAST TO GENERIC
