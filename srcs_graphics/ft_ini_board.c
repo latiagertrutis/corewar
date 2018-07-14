ae510f3a6de048afd4629a19d7dced245836d600
Jaume GARCIA <jagarcia@e2r12p10.42.fr>
2018-07-14 11:47:53 +0200

    rejilla

---
+++ srcs_graphics/ft_ini_board.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ini_board.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jagarcia <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/14 11:09:42 by jagarcia          #+#    #+#             */
+/*   Updated: 2018/07/14 11:47:24 by jagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+
+void		ft_ini_board(t_sdl *Graph, char board[MEM_SIZE])
+{
+	int	font_dim[2];
+	
+	TTF_SizeText(Graph->font, "A", font_dim, font_dim + 1);
+}
